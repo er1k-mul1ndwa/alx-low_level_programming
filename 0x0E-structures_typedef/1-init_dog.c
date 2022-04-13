a24df9a24d303757279a8f4125f82c7c72c13d03
Eric Mulindwa <mulindwaeric577@gmail.com>
2022-04-13 20:13:43 +0000

    My commit message

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,3 +1,4 @@
+#include <stdlib.h>
 #include "dog.h"
 /**
  * init_dog - a function that initialises a variable of type struct dog
@@ -11,7 +12,14 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	d->name = name;
-	d->age = age;
-	d->owner = owner;
+	if (d == NULL)
+		return;
+
+	else
+	}
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+
 }
