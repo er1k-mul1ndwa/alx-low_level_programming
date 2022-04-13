fcede14ed549788f090019015c92e6fe4923e75f
Eric Mulindwa <mulindwaeric577@gmail.com>
2022-04-13 21:05:44 +0000

    My commit message

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,66 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * _strdup - function that returns a pointer to a newly allocated space in
+ * memory, which contains a copy of the string given as a parameter
+ * @str: array
+ * Return: pointer to a new string, NULL if str = NULL, on success returns a
+ * pointer to the duplicated string
+ */
+
+char *_strdup(char *str)
+{
+	int i = 0, len = 0;
+	char *p;
+
+	if (str == NULL)
+		return (NULL);
+	while (str[len] != '\0')
+		len++;
+	p = malloc((len + 1) * sizeof(char));
+	if (p == NULL)
+		return (NULL);
+	for (i = 0; i < len; i++)
+		p[i] = str[i];
+	p[i] = '\0';
+	return (p);
+}
+
+/**
+ * new_dog - function that creates a new dog
+ *
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner name
+ *
+ * Return: a pointer to the type of dog_t -> struct dog
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *dog;
+	char *tmp1, *tmp2;
+
+	dog = malloc(sizeof(dog_t));
+	if (dog == NULL)
+		return (NULL);
+	tmp1 = _strdup(name);
+	if (!tmp1)
+	{
+		free(dog);
+		return (NULL);
+	{
+	tmp2 = _strdup(owner);
+	if (!tmp2)
+	{
+		free(tmp1);
+		free(dog);
+		return (NULL);
+	}
+
+	dog->name = tmp1;
+	dog->owner = tmp2;
+	dog->age = age;
+	return (dog);
+}
