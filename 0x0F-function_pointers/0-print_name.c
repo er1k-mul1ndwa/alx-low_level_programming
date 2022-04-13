f2e22b7d1f0052a45cff4372f642d7c3edce1124
Eric Mulindwa <mulindwaeric577@gmail.com>
2022-04-13 09:13:01 +0000

    My commit message

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - function that prints a name
+ *
+ * @name: string argument
+ * @f: pointer to a function
+ *
+ * Return: void
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (f)
+		f(name);
+}
